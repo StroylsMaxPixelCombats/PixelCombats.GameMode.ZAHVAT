55426e4715429a217c412e68a69bfea991475f6d
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2024-12-04 11:33:42 +0700

    Update default_timer.js

--- client/room/default_timer.js
+++ client/room/default_timer.js
@@ -9,9 +9,9 @@ const PARAMETER_GAME_LENGTH = 'default_game_mode_length';
 export function game_mode_length_seconds() {
     const length = GameMode.Parameters.GetString(PARAMETER_GAME_LENGTH);
     switch (length) {
-        case 'Length_S': return 600; // 6 min
+        case 'Length_S': return 200; // 2 min
         case 'Length_M': return 400; // 4 min
-        case 'Length_L': return 200; // 2 min
+        case 'Length_L': return 600; // 6 min
     }
     return team;
 }
