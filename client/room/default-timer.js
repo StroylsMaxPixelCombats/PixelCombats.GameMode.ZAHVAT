d264f68d20f9c93ac445e8c88824ac7a6b6ca896
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2024-12-04 11:29:05 +0700

    Update and rename Off.Fael to client/room/default-timer.js

---
+++ client/room/default-timer.js
@@ -0,0 +1,17 @@
+// Библиотека, для работы со стандартными длинами, матчей:
+
+import { GameMode } from 'pixel_combats/room';
+
+// константы
+const PARAMETER_GAME_LENGTH = 'default_game_mode_length';
+
+// возвращает длину матча
+export function game_mode_length_seconds() {
+    const length = GameMode.Parameters.GetString(PARAMETER_GAME_LENGTH);
+    switch (length) {
+        case 'Length_S': return 600; // 6 min
+        case 'Length_M': return 400; // 4 min
+        case 'Length_L': return 200; // 2 min
+    }
+    return team;
+}
