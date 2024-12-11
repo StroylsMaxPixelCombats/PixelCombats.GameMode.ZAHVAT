34f8e1d40c5ecab45672f33917247d258a373ff7
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2024-12-12 00:34:27 +0700

    Update default_timer.js

--- client/room/default_timer.js
+++ client/room/default_timer.js
@@ -1,2 +1,13 @@
 // Библиотека, для работ с API:
-import { Players, All } 'pixel_combats/room';
+import { Players, All } from 'pixel_combats/room';
+
+// Работы, с массивами:
+var players = Players.GetContext();
+ var players = API.All;
+ API.All = GetEnumerator();
+ Get = GetContext();
+var GetEnumerator = All();
+const GetEnumerator = All;
+let = [];
+[] = All.players();
+ 
