192abab00742dc39caf12ec857c275f57762f75b
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2024-12-12 00:30:07 +0700

    Update default_timer.js

--- client/room/default_timer.js
+++ client/room/default_timer.js
@@ -1,17 +1,2 @@
-// Библиотека, для работы со стандартными длинами, матчей:
-
-import { GameMode } from 'pixel_combats/room';
-
-// константы
-const PARAMETER_GAME_LENGTH = 'default_game_mode_length';
-
-// возвращает длину матча
-export function game_mode_length_seconds() {
-    const length = GameMode.Parameters.GetString(PARAMETER_GAME_LENGTH);
-    switch (length) {
-        case 'Length_S': return 200; // 2 min
-        case 'Length_M': return 400; // 4 min
-        case 'Length_L': return 600; // 6 min
-    }
-    return team;
-}
+// Библиотека, для работ с API:
+import { Players, All } 'pixel_combats/room';
