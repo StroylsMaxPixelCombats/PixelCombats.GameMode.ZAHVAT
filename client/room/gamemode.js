a9b49215d7ed8730f970ba803b87acefc083c51c
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2025-01-16 15:47:18 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,3 +1,40 @@
-// Авторы:
- By: TnT
-JavaScript.CAPTURE = GameMode;
+/* By: */
+   TnT
+  JavaScript.CAPTURE = GameMode;
+
+/* All, constants - capture:*/
+// Time, constants:
+const WITING_PLAYERS_TIME = 11;
+const BUILD_MODE_TIME = 31;
+const GAME_MODE_TIME = 601;
+const END0F_MATCH_TIME = 11;
+const DEF_TIMER_TICK_IN_DERVAL = 1;
+const DEF_POINTS = GAME_MODE_TIME * 0.2;
+const 
+// Color, constans:
+const BlUE_COLOR = NEW_Color(0, 0, 1, 0);
+const FACKE_CAPTURE_RED_COLOR = NEW_Color(1, 0, 0, 0);
+const CAPTURE_ZONES_COLOR = NEW_Color(1, 1, 1, 0);
+// Max, constans:
+const MAX_SPAWNS_BY_AREA = 25;
+const DEF_POINTS_MAX_COUNT = 30;
+const MAX_CAPTURE_POINTS = 15;
+// Constants - room:
+const RED_CAPTURE_W = 1;
+const BLUE_CAPTURE_W = 2;
+const CAPTURE_RESTORE_W = 1;
+const CAPTURE_POINTS = 10;
+const REPAIR_POINTS_BY_SECOND = 0.5;
+const SAVE_POINTS_COUNT = 10;
+
+/* All, constants - name: */
+const WITING_PLAYERS_STATE_VALUE = "WaitingPLAYERS";
+const 
+
+
+
+
+
+
+
+
