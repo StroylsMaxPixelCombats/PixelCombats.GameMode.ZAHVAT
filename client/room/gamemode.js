fd4da569a37cabdf4fdd1dd2fd1304af6cb20c88
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2025-01-17 13:10:45 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,42 +1,40 @@
-/* By: */
-   TnT
-  JavaScript.CAPTURE = GameMode;
+// Авторы:
+TnT.
+JavaScript.Capture = ("PixelCombats");
 
-/* All, constants - capture:*/
-// Time, constants:
-const WITING_PLAYERS_TIME = 11;
-const BUILD_MODE_TIME = 31;
-const GAME_MODE_TIME = 601;
-const END0F_MATCH_TIME = 11;
-const DEF_TIMER_TICK_IN_DERVAL = 1;
-const DEF_POINTS = GAME_MODE_TIME * 0.2;
-// Color, constans:
-const BlUE_COLOR = NEW_Color(0, 0, 1, 0);
-const FACKE_CAPTURE_RED_COLOR = NEW_Color(1, 0, 0, 0);
-const CAPTURE_ZONES_COLOR = NEW_Color(1, 1, 1, 0);
-// Max, constans:
-const MAX_SPAWNS_BY_AREA = 25;
-const DEF_POINTS_MAX_COUNT = 30;
-const MAX_CAPTURE_POINTS = 15;
-// Constants - room:
-const RED_CAPTURE_W = 1;
-const BLUE_CAPTURE_W = 2;
-const CAPTURE_RESTORE_W = 1;
-const CAPTURE_POINTS = 10;
-const REPAIR_POINTS_BY_SECOND = 0.5;
-const SAVE_POINTS_COUNT = 10;
+// Импорты:
+import { DisplayValueHeader, Color } from "pixel_combats/basic";
+import { Players, Inventory, Teams, Build, Ui, Game, Build, Properties, LeaderBoard, Damage, GameMode, Spawns, Timers, BreackGraph, TeamsBalancer, BuildBlocksSet, AreaPlayerTriggerService, AreaViewService } from "pixel_combats/room";
 
-/* All, constants - name: */
-const WITING_PLAYERS_STATE_VALUE = "WaitingPLAYERS";
-const BUILD_MODE_STATE_VALUE = "BuildMODE";
-const GAME_MODE_STATE_VALUE = "GAME";
-const 
+// Настройки, создания - комнаты:
+Damage.FriendlyFire = GameMode.Parameters.GetBool("FriendlyFire");
+BreackGraph.OnlyPlayerBlocksDmg = GameMode.Parameters.GetBool("PartialDesruction");
+BreackGraph.WeakBlocks = GameMode.Parameters.GetBool("LoosenBlocks");
+Map.Rotation = GameMode.Parameters.GetBool("MapRotation");
 
+// Настройки, игры:
+BreackGraph.PlayerBlockBoost = true;
+Properties.GetContext().GameModeName.Value = "GameModes/Capture The Blue Dots, Or Defend Them!";
+TeamsBalancer.IsAutoBalance = true;
+Ui.GetContext().MainTimerId.Value = mainTimer.Id;
+// Стандартные - команды:
+Teams.Add("Blue", "<b><size=30><color=#0d177c>ß</color><color=#03088c>l</color><color=#0607b0>ᴜ</color><color=#1621ae>E</color></size></b>", new Color(0, 0, 1, 0));
+Teams.Add("Red", "<b><size=30><color=#962605>尺</color><color=#9a040c>ᴇ</color><color=#b8110b>D</color></size></b>", new Color(1, 0, 0, 0));
+var BlueTeam = Teams.Get("Blue");
+var RedTeam = Teams.Get("Red");
+BlueTeam.Spawns.SpawnPointsGroups.Get(1);
+RedTeam.Spawns.SpawnPointsGroups.Get(2);
+BlueTeam.Build.BlocksSet.Value = BuildBlocksSet.Blue;
+RedTeam.Build.BlocksSet.Value = BuildBlocksSet.Red;
 
-
-
-
-
+// Таймер, спавна - для команд:
+BlueTeam.Spawns.RespawnTime.Value = 5;
+RedTeam.Spawns.RespawnTime.Value = 10;
+// Максимальное, количество - очков команды, синих - красных:
+var MaxDeaths = Players.MaxCount * 5;
+BlueTeam.Properties.Get("Deaths").Value = DefPoints;
+RedTeam.Properties.Get("Deaths").Value = MaxDeaths;
+// Выводим, в лидерБордах:
 
 
 
