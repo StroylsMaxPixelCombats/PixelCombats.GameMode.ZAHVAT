d90f531b1dc147b1ec7b989d090c825ab18b0839
TɴTᴡ Oғғɪᴄɪᴀʟ - (GɪᴛHᴜʙ) PIXELCOMBATS 2 <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2025-02-16 09:35:40 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,8 +1,6 @@
-// Импорты:
 import * as Basic from 'pixel_combats/basic';
 import * as Room from 'pixel_combats/room';
 
-// Создаём, команды:
 Room.Teams.Add('Blue', '<b><size=30><color=#0d177c>ß</color><color=#03088c>l</color><color=#0607b0>ᴜ</color><color=#1621ae>E</color></size></b>', new Basic.Color(0, 0, 1, 0));
 Room.Teams.Add('Red', '<b><size=30><color=#962605>尺</color><color=#9a040c>ᴇ</color><color=#b8110b>D</color></size></b>', new Basic.Color(1, 0, 0, 0));
 let RedTeam = Room.Teams.Get('Red');
@@ -12,19 +10,16 @@ RedTeam.Spawns.SpawnPointsGroups.Add(2),
 BlueTeam.Build.BlocksSet.Value = Room.BuildBlocksSet.Blue,  
 RedTeam.Build.BlocksSet.Value = Room.Build.BlocksSet.Red;
 
-// Параметры, создания - комнаты;
 Room.Map.Rotation = Room.GameMode.Parameters.GetBool('MapRotation');
 Room.BreackGraph.OnlyPlayerBlocksDmg = Room.GameMode.Parameters.GetBool('PartialDesruction');
 Room.BreackGraph.WeakBlocks = Room.GameMode.Parameters.GetBool('LoosenBlocks');
 Room.Damage.GetContext().FriendlyFire.Value = Room.GameMode.Parameters.GetBool('FriendlyFire');
-// Опции, включенные - при создании:
-Room.BreackGraph.PlayerBlockBoost = true; // Блок, игрока - всегда, усилен.
-Room.Damage.GetContext().DamageOut.Value = true; // Урон.
-Room.Properties.GetContext().GameModeName.Value = 'GameModes/Team Dead Match'; // Название, режима.
-Room.TeamsBalancer.IsAutoBalance = true; // Авто, баланс - команд.
-Room.Ui.GetContext().MainTimerId.Value = MainTimer.Id; // Индификатор, времени.
+Room.BreackGraph.PlayerBlockBoost = true; 
+Room.Damage.GetContext().DamageOut.Value = true; 
+Room.Properties.GetContext().GameModeName.Value = 'GameModes/Team Dead Match';
+Room.TeamsBalancer.IsAutoBalance = true; 
+Room.Ui.GetContext().MainTimerId.Value = MainTimer.Id; 
 
-// Константы, с переменными:
 const WaitingPlayersTime = 11;
 const BuildModeTime = 61;
 const GameModeTime = 301;
@@ -50,8 +45,24 @@ const GameStateValue = 'Game';
 const End0fMatchStateValue = 'End0fMatch';
 const DefAreaTag = 'Def';
 const CaptureAreaTag = 'Capture';
-const CaptureZoneBlueHINT = '!Красные, захватывают - синию: зону!';
-const 
+const CaptureZoneBlueHint = '!Красные, захватывают - синию зону!';
+const CaptureTeamsBlueHint = '!Защитите, синию зону - с белыми, зонами!';
+const CaptureZoneRedHint = '!Захватывайте, синию - зону, и атакуйте всех - синих!';
+const CaptureTeamsRedHint = '!Захватите, синию зону, с белыми - зонами!';
+
+const MainTimer = Room.Timers.GetContext().Get('Main');
+const DefTickTimer = Room.Timers.GetContext().Get('DefTimer');
+const StateProp = Room.Properties.GetContext().Get('State');
+const DefAreas = Room.AreaService.GetByTag(DefAreaTag);
+const CaptureAreas = Room.AreaService.GetByTag(CaptureAreaTag);
+let CaptureTriggers = [];
+let CaptureViews = [];
+let CaptureProperties = [];
+let CapturedAreaIndexProp = Room.Properties.GetContext().Get('RedCaptiredIndex');
+
+
+
+
 
 
 
